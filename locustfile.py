cec5063da4d85f03c0bdf5edf990fb4a07c62a73
alexisdrd <113307305+alexisdrd@users.noreply.github.com>
2024-11-12 16:18:26 -0500

    main.yml added

--- locustfile.py
+++ locustfile.py
@@ -1,32 +1,58 @@
 from locust import HttpUser, TaskSet, task, between
 
+# Définition des tâches de SimpleUser
 class SimpleTasks(TaskSet):
     @task(1)
     def home(self):
-        # Test the home route
         self.client.get("/")
 
     @task(2)
     def fast(self):
-        # Test the fast route
         self.client.get("/fast")
 
     @task(3)
     def medium(self):
-        # Test the medium route
         self.client.get("/medium")
 
     @task(4)
     def heavy(self):
-        # Test the heavy route
         self.client.get("/heavy")
 
     @task(1)
     def echo(self):
-        # Test the echo route
+        payload = {"message": "Hello, Locust!"}
+        self.client.post("/echo", json=payload)
+
+# Définition des tâches de HeavyUser
+class HeavyTasks(TaskSet):
+    @task(1)
+    def home(self):
+        self.client.get("/")
+
+    @task(2)
+    def fast(self):
+        self.client.get("/fast")
+
+    @task(3)
+    def medium(self):
+        self.client.get("/medium")
+
+    @task(4)
+    def heavy(self):
+        self.client.get("/heavy")
+
+    @task(1)
+    def echo(self):
         payload = {"message": "Hello, Locust!"}
         self.client.post("/echo", json=payload)
 
 class SimpleUser(HttpUser):
     tasks = [SimpleTasks]
-    wait_time = between(1, 5)  # Simulate a wait time between requests
+    wait_time = between(1, 5)  # Temps d'attente entre chaque requête pour SimpleUser
+    weight = 2
+
+class HeavyUser(HttpUser):
+    tasks = [HeavyTasks]
+    wait_time = between(5, 10)  # Temps d'attente entre chaque requête pour HeavyUser
+    weight = 98
+
